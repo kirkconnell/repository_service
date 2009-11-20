8e09500b76ac9830b167985b6fe464895f398d7d
U-Murali-PC\Murali <Murali@Murali-PC.(none)>
2009-11-20 15:53:32 -0600

    clause translation completed

--- spec/repository_service/translation_helper_spec.rb
+++ spec/repository_service/translation_helper_spec.rb
@@ -19,19 +19,19 @@ pV24BPuAEjIJfa1gVwIDAQAB"
       th = TestTranslationHelper.new
       th.context_name(@pk).should == "rsa_b2fcee5a72a0f5674d6537e870d71543" 
     end
-
+    
     it "should translate a single atom clause to an imported single atom clause" do
       th = TestTranslationHelper.new
       clause_node = Controller.parse_credentials(CRED_MSG).certs.first[:clauses][2]
       th.translate_clause(@pk, clause_node).should == "says(rsa_b2fcee5a72a0f5674d6537e870d71543, allow(jeremiah, read, file1)).".delete(" ")
     end
-
+    
     it "should translate an implication clause to an imported implication clause" do
       th = TestTranslationHelper.new
       clause_node = Controller.parse_credentials(CRED_MSG).certs.first[:clauses].first
       th.translate_clause(@pk, clause_node).should == "says(rsa_b2fcee5a72a0f5674d6537e870d71543, allow(PK, Access, Resources)) :- says(rsa_b2fcee5a72a0f5674d6537e870d71543, pk_bind(P, PK)), says(rsa_b2fcee5a72a0f5674d6537e870d71543, allow(P, Access, Resources)).".delete(" ")
     end
-
+    
     it "should not import single clauses from other contexts" do
       th = TestTranslationHelper.new
       clause_node = Controller.parse_credentials(IMP_CRED_MSG).certs.first[:clauses].first
@@ -41,7 +41,19 @@ pV24BPuAEjIJfa1gVwIDAQAB"
     it "should import implications with imported clauses on a non head node" do
       th = TestTranslationHelper.new
       clause_node = Controller.parse_credentials(IMP_CRED_MSG).certs.first[:clauses][1]
-      th.tranlate_clause(@pk, clause_node).should == "says(rsa_b2fcee5a72a0f5674d6537e870d71543, allow(PK, Access, Resources)) :- says(rsa_b2fcee5a72a0f5674d6537e870d71543, pk_bind(P, PK)), says(rsa_example, allow(P, Access, Resources)).".delete(" ")
+      th.translate_clause(@pk, clause_node).should == "says(rsa_b2fcee5a72a0f5674d6537e870d71543, allow(PK, Access, Resources)) :- says(rsa_b2fcee5a72a0f5674d6537e870d71543, pk_bind(P, PK)), says(rsa_example, allow(P, Access, Resources)).".delete(" ")
+    end
+                
+    it "should import implications with imported clauses on a non head node even if the first non head clause is imported" do
+      th = TestTranslationHelper.new
+      clause_node = Controller.parse_credentials(IMP_CRED_MSG).certs.first[:clauses][2]
+      th.translate_clause(@pk, clause_node).should == "says(rsa_b2fcee5a72a0f5674d6537e870d71543, allow(PK, Access, Resources)) :- says(rsa_example, allow(P, Access, Resources)), says(rsa_b2fcee5a72a0f5674d6537e870d71543, pk_bind(P, PK)).".delete(" ")
+    end
+		
+    it "should not import implications with the head node that comes from another context" do
+      th = TestTranslationHelper.new
+      clause_node = Controller.parse_credentials(IMP_CRED_MSG).certs.first[:clauses][3]
+      th.translate_clause(@pk, clause_node).should be_nil
     end
 
   end
